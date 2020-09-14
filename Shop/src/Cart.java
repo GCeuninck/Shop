10852ce98f664ef02d55a1fa278a690f00d37427
GCeuninck <guillaume.ceuninck@etu.imt-lille-douai.fr>
2020-09-15 00:57:03 +0200

    create cart methods

--- Shop/src/Cart.java
+++ Shop/src/Cart.java
@@ -1,4 +1,37 @@
+import java.util.*;
 
 public class Cart {
 
+	Cart(){
+		
+	}
+
+
+	protected void clear() {
+		
+	}
+	
+	protected void add(CartItem item) {
+		
+	}
+	
+	protected void remove(CartItem item) {
+		
+	}
+	
+	protected int quantityProduct(Product p) {
+		int res = 0; 
+		return res;
+	}
+	
+	protected List<CartItem> priceGreaterThan(int amount){
+		final List<CartItem> res = new ArrayList<>();
+		return res;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s", this);
+	}
+	
 }
